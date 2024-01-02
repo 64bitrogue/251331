e3e98ba256efae2826fffefd229681fac970f191
chazz-manubay <155450024+64bitrogue@users.noreply.github.com>
2024-01-02 22:36:25 +0800

    Modified the database structure.

--- database.sql
+++ database.sql
@@ -1,6 +1,9 @@
 CREATE TABLE residents(
     resident_code VARCHAR(255) NOT NULL PRIMARY KEY,
-    resident_name VARCHAR(128) NOT NULL,
+    family_name VARCHAR(50) NOT NULL,
+    given_name VARCHAR(50) NOT NULL,
+    middle_name VARCHAR(50) NOT NULL,
+    civil_status VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
     avg_monthly_salary DECIMAL(19, 4) NOT NULL
 );
