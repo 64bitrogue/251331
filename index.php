a85a8044c5f16118f96dc7d58210c4d385177447
chazz-manubay <155450024+64bitrogue@users.noreply.github.com>
2024-01-02 21:07:08 +0800

    Added content in index page.

--- index.php
+++ index.php
@@ -6,6 +6,43 @@
     <title>251331</title>
 </head>
 <body>
-    
+    <header>
+        <div>
+            <h1>Name</h1>
+            <h2>Group #</h2>
+            <p>Feel free to remove this header.</p>
+        </div>
+    </header>
+    <main>
+        <section>
+            <a href="add-resident.php">+ New Resident</a>
+            <a href="transaction.php">Transaction</a>
+        </section>
+        <section>
+            <table>
+                <thead>
+                    <th>Resident ID</th>
+                    <th>Name</th>
+                    <th>Date of Birth</th>
+                    <th>Average Family Monthly Salary</th>
+                    <th>Actions</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>
+                            <a href="edit.php">Edit</a>
+                            <form onsubmit="confirm('Sure?')" action="delete.php" method="post">
+                                <button type="submit" name="delete">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </main>
 </body>
 </html>
